ce2ab261a90d672aec21cc0622861b59ae157e50
Dorota Szwed <dorota.soroko@gmail.com>
2017-10-01 21:34:31 +0200

    zmodyfikowane zadanie z układem współrzednym

--- src/PositionXY.java
+++ src/PositionXY.java
@@ -4,6 +4,7 @@ public class PositionXY {
     public static void main(String[] args) {
         double x;
         double y;
+        int quarter;
 
         Scanner input = new Scanner(System.in);
         System.out.println("Podaj x: ");
@@ -12,16 +13,18 @@ public class PositionXY {
         y= input.nextDouble();
 
         if (x>0 && y>0){
-            System.out.println("Punkt"+" ( "+ x + ","+y+ " ) "+ "leży"+" I ćwiartce układu współrzędnych");
+            quarter = 1;
         }
         else if (x<0&&y>0){
-            System.out.println("Punkt"+" ( "+ x + ","+y+ " ) "+ "leży"+" II ćwiartce układu współrzędnych");
+            quarter = 2;
         }
         else if (x<0 && y<0){
-            System.out.println("Punkt"+" ( "+ x + ","+y+ " ) "+ "leży"+" III ćwiartce układu współrzędnych");
+            quarter = 3;
         }
         else {
-            System.out.println("Punkt"+" ( "+ x + ","+y+ " ) "+ "leży"+" IV ćwiartce układu współrzędnych");
+            quarter = 4;
         }
+        System.out.println("Punkt"+" ( "+ x + ","+y+ " ) "+ "leży "+ quarter + " ćwiartce układu współrzędnych");
     }
+
 }
