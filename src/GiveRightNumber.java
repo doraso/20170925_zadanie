937833d298f03c6ee6df8492fa35f95eaa143a14
Dorota Szwed <dorota.soroko@gmail.com>
2017-10-01 21:10:04 +0200

    zmodyfikowane zadanie 6

--- src/GiveRightNumber.java
+++ src/GiveRightNumber.java
@@ -3,22 +3,26 @@ import java.util.Scanner;
 public class GiveRightNumber {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int number;
-        for (int i = 0; i < 1000000000; i++) {
+        int number = 0;
+        boolean tmp = true;
+
+        do {
             System.out.println("Podaj liczbę: ");
             number = input.nextInt();
-            if (number > 100 && number < 200) {
-                int result = number / 3;
-                System.out.println("Podana liczba jest podzielna przez 3");
-            } else if (number < 100) {
+            if (number < 100) {
                 System.out.println("Podana liczba jest za mała");
             } else if (number > 200) {
                 System.out.println("Podana liczba jest za duża");
+            } else if (number % 3 != 0) {
+                System.out.println("Podana liczba jest podzielna przez 3");
             } else {
-                System.out.println("Podana liczba nie jest podzielna przez 3");
+                tmp = false;
+                System.out.println("Podana liczba jest prawidłowa");
             }
-
         }
+        while (tmp);
+
     }
 }
+
 
